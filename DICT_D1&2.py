259267186cb125dc42bc66473a2b0298c7db751b
ctian5505 <66417615+ctian5505@users.noreply.github.com>
2023-09-22 21:16:57 +0800

    Update DICT_D1&2.py

--- DICT_D1&2.py
+++ DICT_D1&2.py
@@ -1,22 +1,21 @@
-##String Formatting Functions
-#Text = "chRisTian eSPInosa"
+## String Formatting Functions
+Text = "chRisTian eSPInosa"
 
-#print(f"Upper: {Text.upper()}")
-#print(f"Lower: {Text.lower()}")
-#print(f"Capitalize: {Text.capitalize()}") # Converts the first character to uppercase
-#print(f"Title: {Text.title()}") # Converts the first character of each word to uppercase
-#print(f"Split: {Text.split()}")
-#print(f"Replace: {Text.replace('i', 'o')}")
-#print(f"Len: {len(Text)}")
+print(f"Upper: {Text.upper()}")
+print(f"Lower: {Text.lower()}")
+print(f"Capitalize: {Text.capitalize()}") # Converts the first character to uppercase
+print(f"Title: {Text.title()}") # Converts the first character of each word to uppercase
+print(f"Split: {Text.split()}")
+print(f"Replace: {Text.replace('i', 'o')}")
+print(f"Len: {len(Text)}")
 
 ## Number Formatting Functions
-#import math as m
-#number = 9.6
-
-#print(round(number))
-#print(m.ceil(number))
-#print(m.floor(number))
-#print(pow(number,3))
+import math as m
+number = 9.6
+print(round(number))
+print(m.ceil(number))
+print(m.floor(number))
+print(pow(number,3))
 
 ## Conditional Operators
 # Equals: a == b
@@ -46,92 +45,90 @@
         # * Range
 
     # Sample while loop
-#a = 1
-#while a <= 100:
-#    print(a)
-#    a += 1
+a = 1
+while a <= 100:
+    print(a)
+    a += 1
 
-#x = 1
-#while x <= 20:
-#    print(f"Python while loop number {x}")
-#    x += 1
+x = 1
+while x <= 20:
+    print(f"Python while loop number {x}")
+    x += 1
 
     # Sample for For Loop
-#names = ["Mike", "Ana", "Jun"]
-#for name in names:
-#    print(name)
+names = ["Mike", "Ana", "Jun"]
+for name in names:
+    print(name)
 
-#for num in range(5,11):
-#    print(num)
+for num in range(5,11):
+    print(num)
 
 
-#x = input("Do you want to add number? Y/y = Yes | N/n = No: ")
-
-#while x == "Y" or x == "y":
-#    n1 = int(input("Enter first number: "))
-#    n2 = int(input("Enter second number: "))
-#    print(n1+n2)
-#    x = input("Do you want to enter again? Y/y = Yes | N/n = No: ")
+x = input("Do you want to add number? Y/y = Yes | N/n = No: ")
+while x == "Y" or x == "y":
+    n1 = int(input("Enter first number: "))
+    n2 = int(input("Enter second number: "))
+    print(n1+n2)
+    x = input("Do you want to enter again? Y/y = Yes | N/n = No: ")
 
 
 ## List
-#numbers = [1,2,3,4,5];
-#food = ["Cake", "Burger", "Fries"]
-
-#print(numbers)
-#print(numbers[1])
-#print(food)
-#print(food[2])
+numbers = [1,2,3,4,5];
+food = ["Cake", "Burger", "Fries"]
+print(numbers)
+print(numbers[1])
+print(food)
+print(food[2])
 
 
 ## Adding object to a list
 
 # Using append()
-#numlist = [1, 2, 3, 4, 5]
-#numlist.append()
-#print(numlist)
+numlist = [1, 2, 3, 4, 5]
+numlist.append()
+print(numlist)
 
 # Using insert
-#male = ["John", "Mike"]
-#male.insert(0,"Chan")
-#print(male)
+male = ["John", "Mike"]
+male.insert(0,"Chan")
+print(male)
 
 # Using Remove
-#male = ["John", "Mike"]
-#male.remove("Mike")
-#print(male)
+male = ["John", "Mike"]
+male.remove("Mike")
+print(male)
 
 # Using pop()
-#male = ["John", "Mike"]
-#male.pop(0)
-#print(male)
+male = ["John", "Mike"]
+male.pop(0)
+print(male)
 
 # Using Sort()
-#numlist = [20,2,33,42,25]
-#numlist.sort()
-#print(numlist)
+numlist = [20,2,33,42,25]
+numlist.sort()
+print(numlist)
     # If want desc numlist.sort(reverser=True)
 
 # Using reverse()
-#numlist = [20,2,33,42,25]
-#numlist.reverse()
-#print(numlist)
+numlist = [20,2,33,42,25]
+numlist.reverse()
+print(numlist)
 
 # Using count()
-#numlist = [25,20,2,33,42,25]
-#counter = numlist.count(25)
-#print(counter)
+numlist = [25,20,2,33,42,25]
+counter = numlist.count(25)
+print(counter)
 
 # Using Index()
-#numlist = [25,20,2,33,42,25]
-#indexPosition = numlist.index(20)
-#print(indexPosition)
+numlist = [25,20,2,33,42,25]
+indexPosition = numlist.index(20)
+print(indexPosition)
 
 #Using count()
-#male = ["Bien", "John"]
-#female = ["Jayde", "Ana"]
-#male.extend(female)
-#print(male)
+male = ["Bien", "John"]
+female = ["Jayde", "Ana"]
+male.extend(female)
+print(male)
 
 
 
